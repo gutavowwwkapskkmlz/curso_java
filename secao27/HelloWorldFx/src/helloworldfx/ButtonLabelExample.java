a539592d7659f460d4ccee3d660e3fca43b7463f
GUSTAVO SANTOS OLIVEIRA <gustavoesantos13@gmail.com>
2026-03-14 12:52:22 -0300

    atividade

--- secao27/HelloWorldFx/src/helloworldfx/ButtonLabelExample.java
+++ secao27/HelloWorldFx/src/helloworldfx/ButtonLabelExample.java
@@ -4,7 +4,6 @@ import javafx.application.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.stage.*;
 
 
 public class ButtonLabelExample extends Application {
